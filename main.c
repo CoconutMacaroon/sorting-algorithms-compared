e43d9ef0970fa52138a539e70d491ae682fd6e03
Arjun VedBrat <45187468+CoconutMacaroon@users.noreply.github.com>
2021-06-15 10:57:38 -0700

    Optimize bubble sort, format code

--- main.c
+++ main.c
@@ -4,10 +4,11 @@
 
 #include "main.h"
 
-#define iterations 25
-
-int main() {
+int main()
+{
     int arr[] = {1, 9, 3, 4};
+    barGraph(arr, 4);
+    printf("\n\n-----\n\n");
     bubbleSort(arr, 4);
     barGraph(arr, 4);
     sleepSeconds(5);
@@ -15,34 +16,43 @@ int main() {
     return 0;
 }
 
-void bubbleSort(int arr[], int arrLen) {
-    for(int i = 0; i < iterations; ++i)
-        for(int j = 0; j < (arrLen - 1); ++j)
-            if(arr[j] > arr[j+1]) {
+void bubbleSort(int arr[], int arrLen)
+{
+    bool swapMade = true;
+    while (swapMade)
+        for (int j = 0; j < (arrLen - 1); ++j)
+            if (arr[j] > arr[j + 1])
+            {
                 int temp = arr[j];
                 arr[j] = arr[j + 1];
-                arr[j+1] = temp;
+                arr[j + 1] = temp;
+                swapMade = true;
             }
+            else
+                swapMade = false;
 }
 
-void barGraph(int arr[], int arrLen) {
-    for(int i = 0; i < arrLen; ++i) {
+void barGraph(int arr[], int arrLen)
+{
+    for (int i = 0; i < arrLen; ++i)
         repeatChar('#', arr[i], true);
-    }
 }
 
-void repeatChar(char c, int times, bool printNewline) {
-    for(int i = 0; i < times; ++i) 
+void repeatChar(char c, int times, bool printNewline)
+{
+    for (int i = 0; i < times; ++i)
         putchar(c);
 
     if (printNewline)
         putchar('\n');
 }
 
-void clearScreen() {
+void clearScreen()
+{
     printf("\033[H\033[J");
 }
 
-void sleepSeconds(int seconds) {
+void sleepSeconds(int seconds)
+{
     usleep(seconds * 1000000);
 }
